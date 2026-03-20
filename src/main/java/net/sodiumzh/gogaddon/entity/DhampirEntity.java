daef10787da05631243558322fa7ed48b8f441fe
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2026-03-20 17:48:21 +0800

    entities

---
+++ src/main/java/net/sodiumzh/gogaddon/entity/DhampirEntity.java
@@ -0,0 +1,18 @@
+package net.sodiumzh.gogaddon.entity;
+
+import gaia.entity.AbstractGaiaEntity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
+
+public class DhampirEntity extends AbstractGaiaEntity {
+
+    public DhampirEntity(EntityType<? extends DhampirEntity> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    public float getBaseDefense() {
+        return 0;
+    }
+}
---
+++ src/main/java/net/sodiumzh/gogaddon/entity/KikimoraEntity.java
@@ -0,0 +1,7 @@
+package net.sodiumzh.gogaddon.entity;
+
+import gaia.entity.AbstractAssistGaiaEntity;
+import gaia.entity.AbstractGaiaEntity;
+
+public class KikimoraEntity extends AbstractAssistGaiaEntity {
+}
