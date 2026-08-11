864e983142dde9846f81b05c45b7c1289c7d0222
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2026-08-11 23:22:57 +0800

    Data
    Remove data wrongly copied from nffgirlsgaia
    Fixed and added loot modifiers

--- src/main/java/net/sodiumzh/gogaddon/GOGAddon.java
+++ src/main/java/net/sodiumzh/gogaddon/GOGAddon.java
@@ -23,7 +23,7 @@ public class GOGAddon {
         bus.addListener(GOGAddonConfigs::loadConfig);
 
         GOGAddonEntityTypes.ENTITY_TYPES.register(bus);
-        GOGAddonItems.ITEMS.register(bus);
+        GOGAddonItems.REG.register(bus);
         GOGAddonEntityAttributes.COLLETION.merge();
         GOGAddonEntityComponents.COLLECTION.merge();
         // Register ourselves for server and other game events we are interested in
--- src/main/java/net/sodiumzh/gogaddon/registry/GOGAddonItems.java
+++ src/main/java/net/sodiumzh/gogaddon/registry/GOGAddonItems.java
@@ -5,15 +5,28 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.sodiumzh.gogaddon.GOGAddon;
+import net.sodiumzh.nfu.item.NFUBlockItem;
 import net.sodiumzh.nfu.item.NFUItem;
 
 public class GOGAddonItems {
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GOGAddon.MOD_ID);
+    public static final DeferredRegister<Item> REG = DeferredRegister.create(ForgeRegistries.ITEMS, GOGAddon.MOD_ID);
 
-    public static final RegistryObject<Item> ANT_CHITIN = ITEMS.register("ant_chitin",
+    public static final RegistryObject<NFUItem> ANT_CHITIN = REG.register("ant_chitin",
         () -> new NFUItem(new Item.Properties()));
-    public static final RegistryObject<Item> ANT_CHITIN_FRAGMENT = ITEMS.register("ant_chitin_fragment",
+    public static final RegistryObject<NFUItem> ANT_CHITIN_FRAGMENT = REG.register("ant_chitin_fragment",
+        () -> new NFUItem(new Item.Properties()));
+    public static final RegistryObject<NFUItem> GHOST_FIRE = REG.register("ghost_fire",
+        () -> new NFUItem(new Item.Properties()));
+    public static final RegistryObject<NFUItem> MYSTERIOUS_BRANCH = REG.register("mysterious_branch",
+        () -> new NFUItem(new Item.Properties()));
+    public static final RegistryObject<NFUItem> SHINY_SCALE = REG.register("shiny_scale",
+        () -> new NFUItem(new Item.Properties()));
+    public static final RegistryObject<NFUItem> ENCHANTED_FEATHER = REG.register("enchanted_feather",
+        () -> new NFUItem(new Item.Properties()));
+    public static final RegistryObject<NFUItem> EXPLOSIVE_CORE = REG.register("explosive_core",
         () -> new NFUItem(new Item.Properties()));
 
+    public static final RegistryObject<NFUBlockItem> ENCHANTED_COBBLESTONE = REG.register("enchanted_cobblestone",
+        () -> new NFUBlockItem(GOGAddonBlocks.ENCHANTED_COBBLESTONE.get(), new Item.Properties()));
 }
