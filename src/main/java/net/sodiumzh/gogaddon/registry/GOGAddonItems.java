3a64c7a6fecc579f3ea6dee8a621c122ef699024
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2026-08-12 03:01:24 +0800

    items

--- src/main/java/net/sodiumzh/gogaddon/registry/GOGAddonItems.java
+++ src/main/java/net/sodiumzh/gogaddon/registry/GOGAddonItems.java
@@ -1,6 +1,11 @@
 package net.sodiumzh.gogaddon.registry;
 
+import gaia.GrimoireOfGaia;
+import gaia.registry.GaiaRegistry;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -8,10 +13,22 @@ import net.sodiumzh.gogaddon.GOGAddon;
 import net.sodiumzh.nfu.item.NFUBlockItem;
 import net.sodiumzh.nfu.item.NFUItem;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+@Mod.EventBusSubscriber(modid = GOGAddon.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class GOGAddonItems {
 
     public static final DeferredRegister<Item> REG = DeferredRegister.create(ForgeRegistries.ITEMS, GOGAddon.MOD_ID);
+    private static List<String> NO_TAB_KEY_LIST = new ArrayList<>();
 
+    public static <T extends Item> RegistryObject<T> registerNoTab(String name, Supplier<? extends T> supplier) {
+        NO_TAB_KEY_LIST.add(name);
+        return REG.register(name, supplier);
+    }
+
+    // Tier 1 Loots
     public static final RegistryObject<NFUItem> ANT_CHITIN = REG.register("ant_chitin",
         () -> new NFUItem(new Item.Properties()));
     public static final RegistryObject<NFUItem> ANT_CHITIN_FRAGMENT = REG.register("ant_chitin_fragment",
@@ -26,7 +43,28 @@ public class GOGAddonItems {
         () -> new NFUItem(new Item.Properties()));
     public static final RegistryObject<NFUItem> EXPLOSIVE_CORE = REG.register("explosive_core",
         () -> new NFUItem(new Item.Properties()));
-
     public static final RegistryObject<NFUBlockItem> ENCHANTED_COBBLESTONE = REG.register("enchanted_cobblestone",
         () -> new NFUBlockItem(GOGAddonBlocks.ENCHANTED_COBBLESTONE.get(), new Item.Properties()));
+
+    // Tier 2 Loots
+    public static final RegistryObject<NFUItem> GOLDEN_SCALE = REG.register("golden_scale",
+        () -> new NFUItem(new Item.Properties()));
+    
+    // Tier 3 Loots
+
+
+
+
+
+
+    @SubscribeEvent
+    public static void onCreativeTabRegister(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTab().equals(GaiaRegistry.GAIA_TAB.get())) {
+            ForgeRegistries.ITEMS.getKeys().stream()
+                .filter(key -> key.getNamespace().equals(GOGAddon.MOD_ID))
+                .filter(key -> !NO_TAB_KEY_LIST.contains(key.getPath()))
+                .forEach(key -> event.accept(() -> ForgeRegistries.ITEMS.getValue(key)));
+        }
+    }
+
 }
